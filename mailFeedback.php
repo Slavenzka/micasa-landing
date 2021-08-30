f28532738d19ca58016961517dec111288905a33
Ivan <i.romanov@protonmail.com>
2021-08-30 15:09:24 +0300

    Updates

---
+++ mailFeedback.php
@@ -0,0 +1,21 @@
+<?php
+
+if (filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+    if (isset($_POST['name']) && isset($_POST['email'])) {
+        $userName = $_POST['name'];
+        $userEmail = $_POST['email'];
+        $userComment = $_POST['content'];
+        $messageSubject = 'Новое сообщение от micasa.club';
+
+        $to = "southern.resorts@gmail.com";
+        $body = "Отримано нове звернення!\r\n";
+        $body .= "Від кого: ".$userName. "\r\n";
+        $body .= "Email звернувся: ".$userEmail. "\r\n";
+        $body .= "Зміст запиту: ".$userComment. "\r\n";
+
+        $headers[] = 'From: micasa.club';
+        $headers[] = 'Reply-To: micasa.club';
+
+        mail($to, $messageSubject, $body, implode("\r\n", $headers));
+    }
+}
